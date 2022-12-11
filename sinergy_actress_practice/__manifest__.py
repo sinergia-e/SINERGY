c41fc9ba0451418bfd48594c77a6d012686a97e6
sinergia-e <73215000+sinergia-e@users.noreply.github.com>
2022-12-11 02:17:13 -0600

    Update __manifest__.py

--- sinergy_actress_practice/__manifest__.py
+++ sinergy_actress_practice/__manifest__.py
@@ -1,7 +1,18 @@
-# -*- coding: utf-8 -*-
+# -*- coding : utf-8 -*-
 
 {
-    'name' : "Modulo de Practica sinergia",
-    'author'  : "Sinergy Systems",
-    'data' : [],    
+
+    'name' : "Modulo de Actrices SINERGIA",
+    'version': '1.0',
+    'category': 'Point of Sale',
+    'sequence': 6,
+    'author': 'Sinergia',
+    'summary': "Modulo para Practicas." ,
+    'description': "Modulo de practica.",
+    'data' : 
+    [
+        'views/views.xml',
+        'security/ir.model.access.csv'
+    ],
+    
 }
